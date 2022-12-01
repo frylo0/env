42eccabe059141fb55310a0b169c0d088bd78b93
fritylo <to.fritylo@gmail.com>
2022-12-01 23:31:40 +0300

    .imwheel fixed horizontal scrolling, fixed prev/next mouse buttons

--- INSTALL.sh
+++ INSTALL.sh
@@ -40,7 +40,7 @@ install_deps
 function after_party() {
    printf "\n\n"
    printf "Now you need to:\n"
-   printf "  * Add command \`imwheel\` to startup. Use \`imwheel -k -q\` to reset scroll speed.\n"
+   printf "  * Add command \`imwheel -b \"4 5\"\` to startup. Use \`imwheel -k -q\` to reset scroll speed.\n"
    printf "  * Install DTL from https://github.com/fritylo/dtl\n"
    printf "  * Install Nvim https://askubuntu.com/questions/430008/how-to-install-neovim-on-ubuntu\n"
 }
