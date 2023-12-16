bf85d21babca8c6954fce6ff5856b02ffd3c9711
frylo <frylo.d.ts@gmail.com>
2023-12-16 22:30:09 +0300

    feat(env): added wp-cli to env deps

--- env/deps.sh
+++ env/deps.sh
@@ -1,7 +1,12 @@
 function install_deps() {
    printf "\nInstalling dependecies...\n\n"
 
-   sudo apt-get install imwheel
+   if command -v "imwheel" &> /dev/null; then
+      echo "Imwheel already installed!"
+   else
+      sudo apt-get install imwheel
+      echo "Imwheel has been installed!"
+   fi
 
    if [[ -z $(sudo cat /etc/shells | grep xonsh) ]]; then
       echo 'Installing Xonsh...';
@@ -14,6 +19,17 @@ function install_deps() {
       echo 'Xonsh has been installed!';
    else
       echo 'Xonsh already installed!';
+   fi
+
+   if command -v "wp" &> /dev/null; then
+      echo "WP-CLI already installed!"
+   else
+      curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar # downloading binary
+      chmod +x wp-cli.phar # allow to execute
+      sudo mv wp-cli.phar /usr/local/bin/wp # move to global binaries
+      sudo chown $(whoami):root /usr/local/bin/wp
+
+      echo "WP-CLI has been installed!"
    fi
 
    printf "\nDeps have been installed!\n\n\n"
